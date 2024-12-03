155fa3e53748c56b180296f2266c2c5883c398cb
kkm5291 <kkm23125291@gmail.com>
2024-12-03 11:41:41 +0900

    [feat] Input - validation 로직 추가

--- src/main/java/baseball/view/Input.java
+++ src/main/java/baseball/view/Input.java
@@ -4,9 +4,29 @@ import java.util.Scanner;
 
 public class Input {
 
-    private static Scanner sc = new Scanner(System.in);
+    private static final String WRONG_LENGTH = "3개의 숫자를 입력해 주세요";
+    private static final String WRONG_NUMBER = "숫자만 입력해 주세요";
 
-    public static String UserInput() {
-        return sc.nextLine();
+    private final Scanner sc;
+
+    public Input() {
+        this.sc = new Scanner(System.in);
+    }
+
+    public String userInput() {
+        String input = sc.nextLine();
+        validateUserInput(input);
+        return input;
+    }
+
+    private void validateUserInput(String userInput) {
+        boolean isValid = userInput.chars().allMatch(Character::isDigit);
+        if (userInput.length() != 3) {
+            throw new IllegalArgumentException(WRONG_LENGTH);
+        }
+
+        if (!isValid) {
+            throw new IllegalArgumentException(WRONG_NUMBER);
+        }
     }
 }
